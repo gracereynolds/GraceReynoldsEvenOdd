c943d16b1d4fd59e013071a660783db518c03580
BT_1N3_20@BKTECH.LOCAL <BT_1N3_20@BKTECH.LOCAL>
2018-09-18 08:45:09 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static String OddIndex(String str1) {
+        String temp1 = "";
+        int c = 0;
+    }
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
